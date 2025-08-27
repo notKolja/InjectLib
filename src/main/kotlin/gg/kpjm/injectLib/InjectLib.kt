d3f4eabdf123e262a48b53a1510d2405173c18ea
Kolja <kolja.menzel@outlook.com>
2025-08-27 22:27:04 +0200

    Initial Commit

---
+++ src/main/kotlin/gg/kpjm/injectLib/InjectLib.kt
@@ -0,0 +1,14 @@
+package gg.kpjm.injectLib
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class InjectLib : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
