2d94742f5a8bf3b01a0f4394014ea1ab675b998f
Kolja <kolja.menzel@outlook.com>
2025-08-27 23:32:52 +0200

    0.0.2 - independent usage

--- src/main/kotlin/gg/kpjm/injectLib/InjectLib.kt
+++ src/main/kotlin/gg/kpjm/injectLib/InjectLib.kt
@@ -5,11 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin
 class InjectLib : JavaPlugin() {
 
     companion object {
+        @JvmStatic
         var prefix = "<dark_gray>[<gold>Inject<green>Lib<dark_gray>]"
-    }
-
-    fun setPrefix(s: String) {
-        prefix = s
     }
 
     override fun onEnable() {
