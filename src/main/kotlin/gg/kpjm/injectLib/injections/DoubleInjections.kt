c4b97872dff31b673c9ec04c47a6bee68238b994
Kolja <kolja.menzel@outlook.com>
2025-08-28 13:38:12 +0200

    0.1.2 - changed DoubleInjections.kt
    - added formatCurrency(c)

--- src/main/kotlin/gg/kpjm/injectLib/injections/DoubleInjections.kt
+++ src/main/kotlin/gg/kpjm/injectLib/injections/DoubleInjections.kt
@@ -8,6 +8,16 @@ class DoubleInjections {
     companion object {
 
         /**
+         * Formats a Double to a Currency format with custom Currency sign
+         * example: 10000,00 formats to 10.000,00€
+         */
+
+        fun Double.formatCurrency(c: Char): String {
+            val formatter = NumberFormat.getCurrencyInstance(Locale.GERMANY)
+            return formatter.format(this).replace('€', c).trim()
+        }
+
+        /**
          * Formats a Double to a Currency format with Euro sign
          * example: 10000,00 formats to 10.000,00€
          */
