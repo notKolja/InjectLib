e4a7c647ef2606f7b690f2293ce472dd530480a7
Kolja <kolja.menzel@outlook.com>
2025-08-30 22:45:54 +0200

    0.1.3 - ItemMetaInjections
    - added formatCurrency(locale)
    - added setMiniMessageDisplayName(s)
    - added setCustomModelData(s)
    - added setCustomModelData(c)
    - added setCustomModelData(b)
    - added hasCustomModelData()
    - added hasCustomModelData(f)
    - added hasCustomModelData(s)
    - added hasCustomModelData(c)
    - added hasCustomModelData(b)

--- src/main/kotlin/gg/kpjm/injectLib/InjectLib.kt
+++ src/main/kotlin/gg/kpjm/injectLib/InjectLib.kt
@@ -1,7 +1,10 @@
 package gg.kpjm.injectLib
 
+import gg.kpjm.injectLib.injections.DoubleInjections.Companion.formatCurrency
 import gg.kpjm.injectLib.injections.IntegerInjections.Companion.translateRoman
+import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.Locale
 
 class InjectLib : JavaPlugin() {
 
