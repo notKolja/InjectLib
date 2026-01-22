3f3bc6d8838c7ccf69ef86942a0a93ff49008d42
Kolja <kolja.menzel@outlook.com>
2026-01-22 21:50:47 +0100

    fixed prefixes being overwritten all the time.

--- src/main/kotlin/gg/kpjm/injectLib/InjectLib.kt
+++ src/main/kotlin/gg/kpjm/injectLib/InjectLib.kt
@@ -5,10 +5,21 @@ import org.bukkit.plugin.java.JavaPlugin
 class InjectLib : JavaPlugin() {
 
     companion object {
-        @JvmStatic
-        var prefix: String = "<dark_gray>[<gold>Inject<green>Lib<dark_gray>]"
-        @JvmStatic
+        private val prefixes = mutableMapOf<JavaPlugin, String>()
+
         var plugin: JavaPlugin? = null
+            set(value) {
+                field = value
+                if (value != null && !prefixes.containsKey(value)) {
+                    prefixes[value] = "<gray>» " // default
+                }
+            }
+
+        var prefix: String
+            get() = plugin?.let { prefixes[it] } ?: "<gray>» "
+            set(value) {
+                plugin?.let { prefixes[it] = value }
+            }
     }
 
     override fun onEnable() {
